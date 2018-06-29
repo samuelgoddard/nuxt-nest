33dbaa90f8d3ab3e158f4e58bba8e4d5bd9fc0c7
Sam Goddard <hello@samgoddard.co.uk>
2018-06-29 19:41:53 +0100

    tidying up some layout code

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav class="m-nav m-nav--horizontal">
-    <!-- 
-    Loop through the nuxt generated router items
-    (https://nuxtjs.org/guide/routing) 
-    -->
-    <nuxt-link class="nav-item" v-for="route in $router.options.routes" :key="route.name" :to="route.path">{{route.name}}</nuxt-link>
-  </nav>
+  <div class="wrapper p-bottom--double@medium">
+    <nav class="m-nav m-nav--horizontal">
+      <!-- 
+      Loop through the nuxt generated router items
+      (https://nuxtjs.org/guide/routing) 
+      -->
+      <nuxt-link class="nav-item" v-for="route in $router.options.routes.slice().reverse()" :key="route.name" :to="route.path" v-if="!/\:/.test(route.path)" >{{ route.name }}</nuxt-link>
+    </nav>
+  </div>
 </template>
