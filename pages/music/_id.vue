b9af751fceb5082725aef7be637596530772a7ad
Sam Goddard <hello@samgoddard.co.uk>
2018-06-29 19:42:55 +0100

    music page async data example

---
+++ pages/music/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="wrapper">
+    <div class="g">
+      <div class="g__col g__col-12 g__col-8@medium">
+        <nuxt-link class="nav-item nav-item--underlined" to="/music">Back to all music</nuxt-link>
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.body }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  // Get the data
+  async asyncData({ params }) {
+    // We can use async/await ES6 feature
+    let { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+    return { post: data }
+  },
+  // Meta stuff / page title
+  head() {
+    return {
+      title: this.post.title
+    }
+  }
+}
+</script>
--- pages/music/index.vue
+++ pages/music/index.vue
@@ -1,9 +1,29 @@
 <template>
   <section class="wrapper">
-      <h1>Music</h1>
-      <p>Music listing page</p>
+    <h1>Music</h1>
+    <p>async data example</p>
+    <ul>
+      <li v-for="(music, index) in music" :key="index">
+        <nuxt-link :to="{ name: 'music-id', params: { id: music.id } }">{{ music.title }}</nuxt-link>
+      </li>
+    </ul>
   </section>
 </template>
 
 <script>
+import axios from 'axios'
+
+export default {
+  // Get the data
+  asyncData({ req, params }) {
+    // We can return a Promise instead of calling the callback
+    return axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => {
+        return { music: res.data.slice(0, 8) }
+      })
+  },
+  head: {
+    title: 'Music'
+  }
+}
 </script>
