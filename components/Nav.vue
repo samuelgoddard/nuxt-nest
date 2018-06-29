2006ed06c7682d5418e058fa8da5a5b2349c18c4
Sam Goddard <hello@samgoddard.co.uk>
2018-06-29 18:56:43 +0100

    nav split into it's own component, loops over router defaults

---
+++ components/Nav.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="m-nav m-nav--horizontal">
+    <!-- 
+    Loop through the nuxt generated router items
+    (https://nuxtjs.org/guide/routing) 
+    -->
+    <nuxt-link class="nav-item" v-for="route in $router.options.routes" :key="route.name" :to="route.path">{{route.name}}</nuxt-link>
+  </nav>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <!-- Nav component -->
+    <Nav/>
+    <!-- Nuxt render area -->
     <nuxt/>
   </div>
 </template>
+
+<script>
+import Nav from '~/components/Nav.vue'
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
